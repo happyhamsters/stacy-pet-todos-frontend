f2200ca6b4ec0b19e37cbd71587f45d63ad1f64e
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2023-10-30 09:38:54 +0100

    #5 fix checkbox styles

--- src/components/checkbox/index.jsx
+++ src/components/checkbox/index.jsx
@@ -2,15 +2,20 @@ import classes from './checkbox.module.css';
 import { useState } from 'react';
 
 const Checkbox = () => {
-  const [value, setValue] = useState(false)
+  const [value, setValue] = useState(false);
   const handleChange = () => {
-    setValue(!value)
-  }
-  return <div> 
-    <input className={classes.checkbox}
-    value={value}
-    onChange={handleChange}
-    type="checkbox"/></div>;
-}
+    setValue(!value);
+  };
+  return (
+    <div className={classes.checkbox}>
+      <input
+        className={classes.icon}
+        value={value}
+        onChange={handleChange}
+        type="checkbox"
+      />
+    </div>
+  );
+};
 
 export default Checkbox;
